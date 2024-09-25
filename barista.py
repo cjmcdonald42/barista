b6bd60826ee2953d3befe23cd300d8f3e27468eb
Charles J McDonald <cjmcdonald42@gmail.com>
2024-09-25 09:01:16 -0400

    Update barista.py with comments

--- barista.py
+++ barista.py
@@ -26,7 +26,7 @@ What I can I get for you today?
  
 Please order one drink and, if you want, one bakery item.
 ''')
-isOrdering = True
+isOrdering = True      # --- Drink Order
 drinkOrder = bakeryOrder = ""
 menuChoice = input("First, what would you like to drink? : ")
 while isOrdering:
@@ -46,12 +46,13 @@ while isOrdering:
         print("Please choose 1, 2, or 3")
         menuChoice = input("What would you like to drink? : ")
 
+# --- Whipped Cream on the drink
 menuChoice = input("and, would you like cream on that? (y/N): ")
 if menuChoice == "y":
     print("Sure, you deserve it!")
     drinkOrder = drinkOrder + " with Whipped Cream on top"
 
-isOrdering = True
+isOrdering = True      # -- Bakery order
 menuChoice = input("And would you like an item from the bakery? : ")
 while isOrdering:
     if menuChoice == "4":
@@ -72,11 +73,11 @@ while isOrdering:
         print("Please choose 4, 5, 6, or 'n' for No.")
         menuChoice = input("Would you like an item from the bakery? : ")
 
+# --- Pause for prep time
 print(" === That'll be ready in just a few minutes...\n")
 
-# Order up!
+# --- Order up!
 print(f"I've got an order up for {customerName}!")
 print(f"That's one {drinkOrder} {bakeryOrder}.\n")
-
 print("That's for visiting WacBucks - please enjoy!\n\n")
 
